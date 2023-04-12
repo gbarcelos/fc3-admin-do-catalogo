aa902550eb44d061c200be1ffa19364d96459d5f
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-04-12 08:11:51 -0300

    Implementando o caso de uso de criação de categoria

---
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,6 @@
+package com.fullcycle.admin.catalogo.application.category.create;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+}
