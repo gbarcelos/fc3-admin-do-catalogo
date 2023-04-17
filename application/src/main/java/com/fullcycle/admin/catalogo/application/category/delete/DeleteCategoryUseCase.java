c379584eccd8b12c62197082cf3be6e0a8fea48d
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-04-17 08:19:05 -0300

    Implementando o caso de uso de delete de categoria

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/category/delete/DeleteCategoryUseCase.java
@@ -0,0 +1,5 @@
+package com.fullcycle.admin.catalogo.application.category.delete;
+
+import com.fullcycle.admin.catalogo.application.UnitUseCase;
+
+public abstract class DeleteCategoryUseCase extends UnitUseCase<String> {}
