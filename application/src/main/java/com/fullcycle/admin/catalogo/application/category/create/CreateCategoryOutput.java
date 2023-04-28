3e00b7a42cd6f8c16ca6f2235b367f280458278d
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-04-28 08:06:46 -0300

    Testes integrados do endpoint de criação de categoria

--- application/src/main/java/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryOutput.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryOutput.java
@@ -3,11 +3,13 @@ package com.fullcycle.admin.catalogo.application.category.create;
 import com.fullcycle.admin.catalogo.domain.category.Category;
 import com.fullcycle.admin.catalogo.domain.category.CategoryID;
 
-public record CreateCategoryOutput(
-        CategoryID id
-) {
+public record CreateCategoryOutput(CategoryID id) {
 
-    public static CreateCategoryOutput from(final Category aCategory) {
-        return new CreateCategoryOutput(aCategory.getId());
-    }
+  public static CreateCategoryOutput from(final CategoryID anId) {
+    return new CreateCategoryOutput(anId);
+  }
+
+  public static CreateCategoryOutput from(final Category aCategory) {
+    return new CreateCategoryOutput(aCategory.getId());
+  }
 }
--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -1,24 +1,68 @@
 package com.fullcycle.admin.catalogo.infrastructure.api;
 
+import static io.vavr.API.Right;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fullcycle.admin.catalogo.ControllerTest;
+import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryOutput;
 import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryUseCase;
+
+import java.util.Objects;
+
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+import com.fullcycle.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ControllerTest(controllers = CategoryAPI.class)
 public class CategoryAPITest {
 
-    @Autowired
-    private MockMvc mvc;
+  @Autowired private MockMvc mvc;
 
-    @MockBean
-    private CreateCategoryUseCase createCategoryUseCase;
+  @Autowired private ObjectMapper mapper;
 
-    @Test
-    public void test(){
+  @MockBean private CreateCategoryUseCase createCategoryUseCase;
 
-    }
+  @Test
+  public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
+    final var expectedName = "Filmes";
+    final var expectedDescription = "A categoria mais assistida";
+    final var expectedIsActive = true;
 
+    final var aInput =
+        new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+    when(createCategoryUseCase.execute(any()))
+        .thenReturn(Right(CreateCategoryOutput.from(CategoryID.from("123"))));
+
+    final var request =
+        post("/categories")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(this.mapper.writeValueAsString(aInput));
+
+    final var response = this.mvc.perform(request).andDo(print());
+
+    response
+        .andExpect(status().isCreated())
+        .andExpect(header().string("Location", "/categories/123"))
+        .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(jsonPath("$.id", equalTo("123")));
+
+    verify(createCategoryUseCase, times(1))
+        .execute(
+            argThat(
+                cmd ->
+                    Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())));
+  }
 }
