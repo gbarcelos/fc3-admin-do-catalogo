58d610ff76096ae79548fb1bfe2eaf3ac58b0453
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-05-01 15:39:02 -0300

    Implementando handler de erro global

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.fullcycle.admin.catalogo.infrastructure.api.controllers;
+
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
+import com.fullcycle.admin.catalogo.domain.validation.Error;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = DomainException.class)
+    public ResponseEntity<?> handleDomainException(final DomainException ex) {
+        return ResponseEntity.unprocessableEntity().body(ApiError.from(ex));
+    }
+
+    record ApiError(String message, List<Error> errors) {
+        static ApiError from(final DomainException ex) {
+            return new ApiError(ex.getMessage(), ex.getErrors());
+        }
+    }
+}
--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -1,5 +1,6 @@
 package com.fullcycle.admin.catalogo.infrastructure.api;
 
+import static io.vavr.API.Left;
 import static io.vavr.API.Right;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -12,6 +13,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fullcycle.admin.catalogo.ControllerTest;
 import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryOutput;
 import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
+import com.fullcycle.admin.catalogo.domain.validation.Error;
+import com.fullcycle.admin.catalogo.domain.validation.handler.Notification;
 import com.fullcycle.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
 import java.util.Objects;
 import org.junit.jupiter.api.Test;
@@ -61,5 +65,71 @@ public class CategoryAPITest {
                     Objects.equals(expectedName, cmd.name())
                         && Objects.equals(expectedDescription, cmd.description())
                         && Objects.equals(expectedIsActive, cmd.isActive())));
+  }
+
+  @Test
+  public void givenAInvalidName_whenCallsCreateCategory_thenShouldReturnNotification() throws Exception {
+    final String expectedName = null;
+    final var expectedDescription = "A categoria mais assistida";
+    final var expectedIsActive = true;
+    final var expectedMessage = "'name' should not be null";
+
+    final var aInput =
+            new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+    when(createCategoryUseCase.execute(any()))
+            .thenReturn(Left(Notification.create(new Error(expectedMessage))));
+
+    final var request = post("/categories")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(this.mapper.writeValueAsString(aInput));
+
+    final var response = this.mvc.perform(request)
+            .andDo(print());
+
+    response.andExpect(status().isUnprocessableEntity())
+            .andExpect(header().string("Location", nullValue()))
+            .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.errors", hasSize(1)))
+            .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)));
+
+    verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+            Objects.equals(expectedName, cmd.name())
+                    && Objects.equals(expectedDescription, cmd.description())
+                    && Objects.equals(expectedIsActive, cmd.isActive())
+    ));
+  }
+  @Test
+  public void givenAInvalidCommand_whenCallsCreateCategory_thenShouldReturnDomainException() throws Exception {
+    final String expectedName = null;
+    final var expectedDescription = "A categoria mais assistida";
+    final var expectedIsActive = true;
+    final var expectedMessage = "'name' should not be null";
+
+    final var aInput =
+            new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+    when(createCategoryUseCase.execute(any()))
+            .thenThrow(DomainException.with(new Error(expectedMessage)));
+
+    final var request = post("/categories")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(this.mapper.writeValueAsString(aInput));
+
+    final var response = this.mvc.perform(request)
+            .andDo(print());
+
+    response.andExpect(status().isUnprocessableEntity())
+            .andExpect(header().string("Location", nullValue()))
+            .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.message", equalTo(expectedMessage)))
+            .andExpect(jsonPath("$.errors", hasSize(1)))
+            .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)));
+
+    verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+            Objects.equals(expectedName, cmd.name())
+                    && Objects.equals(expectedDescription, cmd.description())
+                    && Objects.equals(expectedIsActive, cmd.isActive())
+    ));
   }
 }
