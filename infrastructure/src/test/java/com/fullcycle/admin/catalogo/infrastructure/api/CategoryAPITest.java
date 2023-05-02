0358a543464e95916810fda33f8a0476ec7f88d3
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-05-02 07:43:29 -0300

    Testes integrados do endpoint de atualização de categoria

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -15,6 +15,8 @@ import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryOu
 import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryUseCase;
 import com.fullcycle.admin.catalogo.application.category.retrieve.get.CategoryOutput;
 import com.fullcycle.admin.catalogo.application.category.retrieve.get.GetCategoryByIdUseCase;
+import com.fullcycle.admin.catalogo.application.category.update.UpdateCategoryOutput;
+import com.fullcycle.admin.catalogo.application.category.update.UpdateCategoryUseCase;
 import com.fullcycle.admin.catalogo.domain.category.Category;
 import com.fullcycle.admin.catalogo.domain.category.CategoryID;
 import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
@@ -23,6 +25,8 @@ import com.fullcycle.admin.catalogo.domain.validation.Error;
 import com.fullcycle.admin.catalogo.domain.validation.handler.Notification;
 import com.fullcycle.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
 import java.util.Objects;
+
+import com.fullcycle.admin.catalogo.infrastructure.category.models.UpdateCategoryApiInput;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -39,6 +43,8 @@ public class CategoryAPITest {
   @MockBean private CreateCategoryUseCase createCategoryUseCase;
 
   @MockBean private GetCategoryByIdUseCase getCategoryByIdUseCase;
+
+  @MockBean private UpdateCategoryUseCase updateCategoryUseCase;
 
   @Test
   public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
@@ -214,5 +220,43 @@ public class CategoryAPITest {
     response
         .andExpect(status().isNotFound())
         .andExpect(jsonPath("$.message", equalTo(expectedErrorMessage)));
+  }
+
+  @Test
+  public void givenAValidCommand_whenCallsUpdateCategory_shouldReturnCategoryId() throws Exception {
+    // given
+    final var expectedId = "123";
+    final var expectedName = "Filmes";
+    final var expectedDescription = "A categoria mais assistida";
+    final var expectedIsActive = true;
+
+    when(updateCategoryUseCase.execute(any()))
+        .thenReturn(Right(UpdateCategoryOutput.from(expectedId)));
+
+    final var aCommand =
+        new UpdateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+    // when
+    final var request =
+        put("/categories/{id}", expectedId)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(aCommand));
+
+    final var response = this.mvc.perform(request).andDo(print());
+
+    // then
+    response
+        .andExpect(status().isOk())
+        .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(jsonPath("$.id", equalTo(expectedId)));
+
+    verify(updateCategoryUseCase, times(1))
+        .execute(
+            argThat(
+                cmd ->
+                    Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())));
   }
 }
