9c09ab304645f499b0697ee0c621be48491a61a4
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-05-02 08:15:47 -0300

    Testes integrados do endpoint de delete de categoria por id

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -13,6 +13,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fullcycle.admin.catalogo.ControllerTest;
 import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryOutput;
 import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.fullcycle.admin.catalogo.application.category.delete.DeleteCategoryUseCase;
 import com.fullcycle.admin.catalogo.application.category.retrieve.get.CategoryOutput;
 import com.fullcycle.admin.catalogo.application.category.retrieve.get.GetCategoryByIdUseCase;
 import com.fullcycle.admin.catalogo.application.category.update.UpdateCategoryOutput;
@@ -45,6 +46,7 @@ public class CategoryAPITest {
   @MockBean private GetCategoryByIdUseCase getCategoryByIdUseCase;
 
   @MockBean private UpdateCategoryUseCase updateCategoryUseCase;
+  @MockBean private DeleteCategoryUseCase deleteCategoryUseCase;
 
   @Test
   public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
@@ -342,5 +344,26 @@ public class CategoryAPITest {
                     Objects.equals(expectedName, cmd.name())
                         && Objects.equals(expectedDescription, cmd.description())
                         && Objects.equals(expectedIsActive, cmd.isActive())));
+  }
+
+  @Test
+  public void givenAValidId_whenCallsDeleteCategory_shouldReturnNoContent() throws Exception {
+    // given
+    final var expectedId = "123";
+
+    doNothing().when(deleteCategoryUseCase).execute(any());
+
+    // when
+    final var request =
+        delete("/categories/{id}", expectedId)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON);
+
+    final var response = this.mvc.perform(request).andDo(print());
+
+    // then
+    response.andExpect(status().isNoContent());
+
+    verify(deleteCategoryUseCase, times(1)).execute(eq(expectedId));
   }
 }
