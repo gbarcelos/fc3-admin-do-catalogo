68086739406bb1e26fd93c76b5d4889d76e215b7
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-05-04 08:00:03 -0300

    Testes e2e da criação de categoria

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/E2ETest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/E2ETest.java
@@ -1,12 +1,10 @@
 package com.fullcycle.admin.catalogo;
 
 import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
-import org.junit.jupiter.api.extension.ExtendWith;
+import java.lang.annotation.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.lang.annotation.*;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
