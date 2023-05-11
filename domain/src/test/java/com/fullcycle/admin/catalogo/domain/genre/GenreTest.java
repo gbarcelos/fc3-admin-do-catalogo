a6d96c8dfef8acbce33c0da8de079298ff413b8d
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-05-11 07:50:45 -0300

    Testes do comportamento de ativar e desativar um genre

--- domain/src/test/java/com/fullcycle/admin/catalogo/domain/genre/GenreTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/genre/GenreTest.java
@@ -47,8 +47,8 @@ public class GenreTest {
     final var expectedErrorMessage = "'name' should not be empty";
 
     final var actualException =
-            Assertions.assertThrows(
-                    NotificationException.class, () -> Genre.newGenre(expectedName, expectedIsActive));
+        Assertions.assertThrows(
+            NotificationException.class, () -> Genre.newGenre(expectedName, expectedIsActive));
 
     Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
     Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
@@ -68,10 +68,63 @@ public class GenreTest {
     final var expectedErrorMessage = "'name' must be between 1 and 255 characters";
 
     final var actualException =
-            Assertions.assertThrows(
-                    NotificationException.class, () -> Genre.newGenre(expectedName, expectedIsActive));
+        Assertions.assertThrows(
+            NotificationException.class, () -> Genre.newGenre(expectedName, expectedIsActive));
 
     Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
     Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+  }
+
+  @Test
+  public void givenAnActiveGenre_whenCallDeactivate_shouldReceiveOK() {
+    final var expectedName = "Ação";
+    final var expectedIsActive = false;
+    final var expectedCategories = 0;
+
+    final var actualGenre = Genre.newGenre(expectedName, true);
+
+    Assertions.assertNotNull(actualGenre);
+    Assertions.assertTrue(actualGenre.isActive());
+    Assertions.assertNull(actualGenre.getDeletedAt());
+
+    final var actualCreatedAt = actualGenre.getCreatedAt();
+    final var actualUpdatedAt = actualGenre.getUpdatedAt();
+
+    actualGenre.deactivate();
+
+    Assertions.assertNotNull(actualGenre.getId());
+    Assertions.assertEquals(expectedName, actualGenre.getName());
+    Assertions.assertEquals(expectedIsActive, actualGenre.isActive());
+    Assertions.assertEquals(expectedCategories, actualGenre.getCategories().size());
+    Assertions.assertEquals(actualCreatedAt, actualGenre.getCreatedAt());
+    Assertions.assertTrue(actualUpdatedAt.isBefore(actualGenre.getUpdatedAt()));
+    Assertions.assertNotNull(actualGenre.getDeletedAt());
+  }
+
+  @Test
+  public void givenAnInactiveGenre_whenCallActivate_shouldReceiveOK() {
+    final var expectedName = "Ação";
+    final var expectedIsActive = true;
+    final var expectedCategories = 0;
+
+    final var actualGenre = Genre.newGenre(expectedName, false);
+
+    Assertions.assertNotNull(actualGenre);
+    Assertions.assertFalse(actualGenre.isActive());
+    Assertions.assertNotNull(actualGenre.getDeletedAt());
+
+    final var actualCreatedAt = actualGenre.getCreatedAt();
+    final var actualUpdatedAt = actualGenre.getUpdatedAt();
+
+    actualGenre.activate();
+
+    Assertions.assertNotNull(actualGenre.getId());
+    Assertions.assertEquals(expectedName, actualGenre.getName());
+    Assertions.assertEquals(expectedIsActive, actualGenre.isActive());
+    Assertions.assertEquals(expectedCategories, actualGenre.getCategories().size());
+    Assertions.assertEquals(actualCreatedAt, actualGenre.getCreatedAt());
+    Assertions.assertTrue(actualUpdatedAt.isBefore(actualGenre.getUpdatedAt()));
+    Assertions.assertNotNull(actualGenre.getUpdatedAt());
+    Assertions.assertNull(actualGenre.getDeletedAt());
   }
 }
