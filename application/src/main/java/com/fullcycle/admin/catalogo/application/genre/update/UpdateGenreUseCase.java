516b40d7ff75893539b77ab90335c873a287635e
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-05-17 08:36:22 -0300

    Implementando o caso de uso de atualização de genre

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/genre/update/UpdateGenreUseCase.java
@@ -0,0 +1,5 @@
+package com.fullcycle.admin.catalogo.application.genre.update;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+
+public abstract class UpdateGenreUseCase extends UseCase<UpdateGenreCommand, UpdateGenreOutput> {}
