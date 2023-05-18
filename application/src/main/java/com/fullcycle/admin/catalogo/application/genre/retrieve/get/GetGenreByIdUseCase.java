5d20a7226bbe31e5eb6fff832dce50b37f5acdb1
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-05-18 20:46:23 -0300

    Implementando o caso de uso de get de genre por id

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/genre/retrieve/get/GetGenreByIdUseCase.java
@@ -0,0 +1,5 @@
+package com.fullcycle.admin.catalogo.application.genre.retrieve.get;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+
+public abstract class GetGenreByIdUseCase extends UseCase<String, GenreOutput> {}
