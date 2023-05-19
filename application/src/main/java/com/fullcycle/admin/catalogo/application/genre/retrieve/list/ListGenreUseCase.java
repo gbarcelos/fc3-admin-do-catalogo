000745a595ed2c5cd451ad1c158ac0d64acb4537
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-05-18 23:04:45 -0300

    Implementando o caso de uso de listagem de genre

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/genre/retrieve/list/ListGenreUseCase.java
@@ -0,0 +1,7 @@
+package com.fullcycle.admin.catalogo.application.genre.retrieve.list;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admin.catalogo.domain.pagination.SearchQuery;
+
+public abstract class ListGenreUseCase extends UseCase<SearchQuery, Pagination<GenreListOutput>> {}
