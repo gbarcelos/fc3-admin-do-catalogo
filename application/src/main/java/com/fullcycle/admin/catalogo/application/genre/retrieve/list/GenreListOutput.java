b18fd694b03462d09ff248cddf383a91c57ee618
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-05-25 08:08:27 -0300

    Criando controller de gênero

--- application/src/main/java/com/fullcycle/admin/catalogo/application/genre/retrieve/list/GenreListOutput.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/genre/retrieve/list/GenreListOutput.java
@@ -6,10 +6,16 @@ import java.time.Instant;
 import java.util.List;
 
 public record GenreListOutput(
-    String name, boolean isActive, List<String> categories, Instant createdAt, Instant deletedAt) {
+    String id,
+    String name,
+    boolean isActive,
+    List<String> categories,
+    Instant createdAt,
+    Instant deletedAt) {
 
   public static GenreListOutput from(final Genre aGenre) {
     return new GenreListOutput(
+        aGenre.getId().getValue(),
         aGenre.getName(),
         aGenre.isActive(),
         aGenre.getCategories().stream().map(CategoryID::getValue).toList(),
