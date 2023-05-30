64b37b50fa378f92d9ec21c461f36b195dc6f314
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-05-30 07:58:43 -0300

    TDD da nova entidade de castmember

---
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/castmember/CastMemberTest.java
@@ -0,0 +1,23 @@
+package com.fullcycle.admin.catalogo.domain.castmember;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CastMemberTest {
+
+    @Test
+    public void givenAValidParams_whenCallsNewMember_thenInstantiateACastMember() {
+        final var expectedName = "Vin Diesel";
+        final var expectedType = CastMemberType.ACTOR;
+
+        final var actualMember = CastMember.newMember(expectedName, expectedType);
+
+        Assertions.assertNotNull(actualMember);
+        Assertions.assertNotNull(actualMember.getId());
+        Assertions.assertEquals(expectedName, actualMember.getName());
+        Assertions.assertEquals(expectedType, actualMember.getType());
+        Assertions.assertNotNull(actualMember.getCreatedAt());
+        Assertions.assertNotNull(actualMember.getUpdatedAt());
+        Assertions.assertEquals(actualMember.getCreatedAt(), actualMember.getUpdatedAt());
+    }
+}
