2ff17cbd93b53ff2f0458884b864c0319b93b5ef
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-05-30 08:23:42 -0300

    Implementando o castmember validator

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/castmember/CastMemberValidator.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/castmember/CastMemberValidator.java
@@ -1,9 +1,13 @@
 package com.fullcycle.admin.catalogo.domain.castmember;
 
+import com.fullcycle.admin.catalogo.domain.validation.Error;
 import com.fullcycle.admin.catalogo.domain.validation.ValidationHandler;
 import com.fullcycle.admin.catalogo.domain.validation.Validator;
 
 public class CastMemberValidator extends Validator {
+
+  private static final int NAME_MAX_LENGTH = 255;
+  private static final int NAME_MIN_LENGTH = 3;
 
   private final CastMember castMember;
 
@@ -13,5 +17,33 @@ public class CastMemberValidator extends Validator {
   }
 
   @Override
-  public void validate() {}
+  public void validate() {
+    checkNameConstraints();
+    checkTypeConstraints();
+  }
+
+  private void checkNameConstraints() {
+    final var name = this.castMember.getName();
+    if (name == null) {
+      this.validationHandler().append(new Error("'name' should not be null"));
+      return;
+    }
+
+    if (name.isBlank()) {
+      this.validationHandler().append(new Error("'name' should not be empty"));
+      return;
+    }
+
+    final int length = name.trim().length();
+    if (length > NAME_MAX_LENGTH || length < NAME_MIN_LENGTH) {
+      this.validationHandler().append(new Error("'name' must be between 3 and 255 characters"));
+    }
+  }
+
+  private void checkTypeConstraints() {
+    final var type = this.castMember.getType();
+    if (type == null) {
+      this.validationHandler().append(new Error("'type' should not be null"));
+    }
+  }
 }
