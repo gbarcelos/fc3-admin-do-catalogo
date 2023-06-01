d7e43fc96f05a8a380bd079f296db29d654a22a8
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-06-01 07:49:06 -0300

    Criando o jpa repository de castmember

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/MySQLCleanUpExtension.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/MySQLCleanUpExtension.java
@@ -1,5 +1,6 @@
 package com.fullcycle.admin.catalogo;
 
+import com.fullcycle.admin.catalogo.infrastructure.castmember.persistence.CastMemberRepository;
 import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import com.fullcycle.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
@@ -19,6 +20,7 @@ public class MySQLCleanUpExtension implements BeforeEachCallback {
 
     cleanUp(
         List.of(
+            appContext.getBean(CastMemberRepository.class),
             appContext.getBean(GenreRepository.class),
             appContext.getBean(CategoryRepository.class)));
   }
