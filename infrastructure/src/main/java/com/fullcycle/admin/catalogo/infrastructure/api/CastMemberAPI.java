c7c7b969ca10adad0935d737b48c2b9633bb283d
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-06-07 07:36:15 -0300

    Implementando o endpoint de get de castmember por id

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/CastMemberAPI.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/CastMemberAPI.java
@@ -1,5 +1,6 @@
 package com.fullcycle.admin.catalogo.infrastructure.api;
 
+import com.fullcycle.admin.catalogo.infrastructure.castmember.models.CastMemberResponse;
 import com.fullcycle.admin.catalogo.infrastructure.castmember.models.CreateCastMemberRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -24,4 +25,14 @@ public interface CastMemberAPI {
         @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
       })
   ResponseEntity<?> create(@RequestBody CreateCastMemberRequest input);
+
+  @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @Operation(summary = "Get a cast member by it's identifier")
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "200", description = "Cast member retrieved"),
+        @ApiResponse(responseCode = "404", description = "Cast member was not found"),
+        @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+      })
+  CastMemberResponse getById(@PathVariable String id);
 }
---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/castmember/models/CastMemberResponse.java
@@ -0,0 +1,4 @@
+package com.fullcycle.admin.catalogo.infrastructure.castmember.models;
+
+public record CastMemberResponse(
+    String id, String name, String type, String createdAt, String updatedAt) {}
