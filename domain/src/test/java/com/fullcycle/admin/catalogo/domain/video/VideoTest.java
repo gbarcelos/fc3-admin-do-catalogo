b4ba0a31003cafeaf694f5b0b64f214b7afb31a7
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-06-10 07:52:38 -0300

    TDD do comportamento de update do video

--- domain/src/test/java/com/fullcycle/admin/catalogo/domain/video/VideoTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/video/VideoTest.java
@@ -69,4 +69,75 @@ public class VideoTest {
 
     Assertions.assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
   }
+
+  @Test
+  public void givenValidVideo_whenCallsUpdate_shouldReturnUpdated() throws InterruptedException {
+    // given
+    final var expectedTitle = "System Design Interviews";
+    final var expectedDescription = """
+                Disclaimer: o estudo de caso apresentado tem fins educacionais e representa nossas opiniões pessoais.
+                Esse vídeo faz parte da Imersão Full Stack && Full Cycle.
+                Para acessar todas as aulas, lives e desafios, acesse:
+                https://imersao.fullcycle.com.br/
+                """;
+    final var expectedLaunchedAt = Year.of(2022);
+    final var expectedDuration = 120.10;
+    final var expectedOpened = false;
+    final var expectedPublished = false;
+    final var expectedRating = Rating.L;
+    final var expectedCategories = Set.of(CategoryID.unique());
+    final var expectedGenres = Set.of(GenreID.unique());
+    final var expectedMembers = Set.of(CastMemberID.unique());
+
+    final var aVideo = Video.newVideo(
+            "Test title",
+            "Lalala description",
+            Year.of(1888),
+            0.0,
+            true,
+            true,
+            Rating.AGE_10,
+            Set.of(),
+            Set.of(),
+            Set.of()
+    );
+
+    Thread.sleep(1);
+    // when
+    final var actualVideo = Video.with(aVideo).update(
+            expectedTitle,
+            expectedDescription,
+            expectedLaunchedAt,
+            expectedDuration,
+            expectedOpened,
+            expectedPublished,
+            expectedRating,
+            expectedCategories,
+            expectedGenres,
+            expectedMembers
+    );
+
+    // then
+    Assertions.assertNotNull(actualVideo);
+    Assertions.assertNotNull(actualVideo.getId());
+    Assertions.assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+    Assertions.assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+    Assertions.assertEquals(expectedTitle, actualVideo.getTitle());
+    Assertions.assertEquals(expectedDescription, actualVideo.getDescription());
+    Assertions.assertEquals(expectedLaunchedAt, actualVideo.getLaunchedAt());
+    Assertions.assertEquals(expectedDuration, actualVideo.getDuration());
+    Assertions.assertEquals(expectedOpened, actualVideo.getOpened());
+    Assertions.assertEquals(expectedPublished, actualVideo.getPublished());
+    Assertions.assertEquals(expectedRating, actualVideo.getRating());
+    Assertions.assertEquals(expectedCategories, actualVideo.getCategories());
+    Assertions.assertEquals(expectedGenres, actualVideo.getGenres());
+    Assertions.assertEquals(expectedMembers, actualVideo.getCastMembers());
+    Assertions.assertTrue(actualVideo.getVideo().isEmpty());
+    Assertions.assertTrue(actualVideo.getTrailer().isEmpty());
+    Assertions.assertTrue(actualVideo.getBanner().isEmpty());
+    Assertions.assertTrue(actualVideo.getThumbnail().isEmpty());
+    Assertions.assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+
+    Assertions.assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+  }
 }
