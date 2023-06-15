15695d286960071c11e37b5172be5aa0d127a642
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-06-15 07:27:30 -0300

    Implementando o caso de uso de delete de vídeo por id

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/video/delete/DeleteVideoUseCase.java
@@ -0,0 +1,5 @@
+package com.fullcycle.admin.catalogo.application.video.delete;
+
+import com.fullcycle.admin.catalogo.application.UnitUseCase;
+
+public abstract class DeleteVideoUseCase extends UnitUseCase<String> {}
