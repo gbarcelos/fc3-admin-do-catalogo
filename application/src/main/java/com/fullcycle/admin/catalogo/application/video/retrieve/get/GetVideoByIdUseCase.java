152299d4eed1166d4a3c025c04bdfa256af5e4b1
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-06-15 08:00:55 -0300

    Implementando o caso de uso de get de vídeo por id

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/video/retrieve/get/GetVideoByIdUseCase.java
@@ -0,0 +1,5 @@
+package com.fullcycle.admin.catalogo.application.video.retrieve.get;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+
+public abstract class GetVideoByIdUseCase extends UseCase<String, VideoOutput> {}
