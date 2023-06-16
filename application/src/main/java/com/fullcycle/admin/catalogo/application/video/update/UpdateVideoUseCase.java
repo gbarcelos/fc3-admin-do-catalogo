01d750cc0aa85e7cd665c6df0ae0a7511ef56759
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-06-16 07:36:54 -0300

    Implementando o caso de uso de atualização de vídeo

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/video/update/UpdateVideoUseCase.java
@@ -0,0 +1,5 @@
+package com.fullcycle.admin.catalogo.application.video.update;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+
+public abstract class UpdateVideoUseCase extends UseCase<UpdateVideoCommand, UpdateVideoOutput> {}
