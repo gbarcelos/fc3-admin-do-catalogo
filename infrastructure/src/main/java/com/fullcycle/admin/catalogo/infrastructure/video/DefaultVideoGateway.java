8ade3abb3fbb7cf730a379aee7c2f5bbbfd94f48
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-06-22 08:32:21 -0300

    Implementando o método create do gateway de video

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/video/DefaultVideoGateway.java
@@ -0,0 +1,46 @@
+package com.fullcycle.admin.catalogo.infrastructure.video;
+
+import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admin.catalogo.domain.video.*;
+import com.fullcycle.admin.catalogo.infrastructure.video.persistence.VideoJpaEntity;
+import com.fullcycle.admin.catalogo.infrastructure.video.persistence.VideoRepository;
+import java.util.Objects;
+import java.util.Optional;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class DefaultVideoGateway implements VideoGateway {
+
+  private final VideoRepository videoRepository;
+
+  public DefaultVideoGateway(final VideoRepository videoRepository) {
+    this.videoRepository = Objects.requireNonNull(videoRepository);
+  }
+
+  @Override
+  @Transactional
+  public Video create(final Video aVideo) {
+    return this.videoRepository.save(VideoJpaEntity.from(aVideo)).toAggregate();
+  }
+
+  @Override
+  public void deleteById(final VideoID anId) {}
+
+  @Override
+  @Transactional(readOnly = true)
+  public Optional<Video> findById(final VideoID anId) {
+    return Optional.empty();
+  }
+
+  @Override
+  @Transactional
+  public Video update(final Video aVideo) {
+    return null;
+  }
+
+  @Override
+  public Pagination<Video> findAll(VideoSearchQuery aQuery) {
+    return null;
+  }
+}
