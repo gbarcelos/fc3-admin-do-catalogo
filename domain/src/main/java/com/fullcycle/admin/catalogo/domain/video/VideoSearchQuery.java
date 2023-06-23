92749529a8c30dbd37fad4c915afe6770f89cad6
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-06-23 08:31:22 -0300

    Implementando o metodo findall do gateway de video

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/video/VideoSearchQuery.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/video/VideoSearchQuery.java
@@ -1,4 +1,17 @@
 package com.fullcycle.admin.catalogo.domain.video;
 
+import com.fullcycle.admin.catalogo.domain.castmember.CastMemberID;
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+import com.fullcycle.admin.catalogo.domain.genre.GenreID;
+
+import java.util.Set;
+
 public record VideoSearchQuery(
-    int page, int perPage, String terms, String sort, String direction) {}
+    int page,
+    int perPage,
+    String terms,
+    String sort,
+    String direction,
+    Set<CastMemberID> castMembers,
+    Set<CategoryID> categories,
+    Set<GenreID> genres) {}
--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/video/DefaultVideoGateway.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/video/DefaultVideoGateway.java
@@ -1,11 +1,17 @@
 package com.fullcycle.admin.catalogo.infrastructure.video;
 
+import com.fullcycle.admin.catalogo.domain.Identifier;
 import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
 import com.fullcycle.admin.catalogo.domain.video.*;
+import com.fullcycle.admin.catalogo.infrastructure.utils.SqlUtils;
 import com.fullcycle.admin.catalogo.infrastructure.video.persistence.VideoJpaEntity;
 import com.fullcycle.admin.catalogo.infrastructure.video.persistence.VideoRepository;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,8 +41,7 @@ public class DefaultVideoGateway implements VideoGateway {
   @Override
   @Transactional(readOnly = true)
   public Optional<Video> findById(final VideoID anId) {
-    return this.videoRepository.findById(anId.getValue())
-            .map(VideoJpaEntity::toAggregate);
+    return this.videoRepository.findById(anId.getValue()).map(VideoJpaEntity::toAggregate);
   }
 
   @Override
@@ -46,12 +51,36 @@ public class DefaultVideoGateway implements VideoGateway {
   }
 
   @Override
-  public Pagination<Video> findAll(VideoSearchQuery aQuery) {
-    return null;
+  public Pagination<VideoPreview> findAll(VideoSearchQuery aQuery) {
+    final var page =
+        PageRequest.of(
+            aQuery.page(),
+            aQuery.perPage(),
+            Sort.by(Sort.Direction.fromString(aQuery.direction()), aQuery.sort()));
+
+    final var actualPage =
+        this.videoRepository.findAll(
+            SqlUtils.like(aQuery.terms()),
+            toString(aQuery.castMembers()),
+            toString(aQuery.categories()),
+            toString(aQuery.genres()),
+            page);
+
+    return new Pagination<>(
+        actualPage.getNumber(),
+        actualPage.getSize(),
+        actualPage.getTotalElements(),
+        actualPage.toList());
+  }
+
+  private Set<String> toString(final Set<? extends Identifier> ids) {
+    if (ids == null || ids.isEmpty()) {
+      return null;
+    }
+    return ids.stream().map(Identifier::getValue).collect(Collectors.toSet());
   }
 
   private Video save(final Video aVideo) {
-    return this.videoRepository.save(VideoJpaEntity.from(aVideo))
-            .toAggregate();
+    return this.videoRepository.save(VideoJpaEntity.from(aVideo)).toAggregate();
   }
 }
