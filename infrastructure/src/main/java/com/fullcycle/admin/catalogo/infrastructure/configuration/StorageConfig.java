0f310acad482eb500a9b368bbe15c3132d91dc5b
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-07-06 07:40:56 -0300

    Configurando beans do storageservice

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/StorageConfig.java
@@ -0,0 +1,28 @@
+package com.fullcycle.admin.catalogo.infrastructure.configuration;
+
+import com.fullcycle.admin.catalogo.infrastructure.configuration.properties.GoogleStorageProperties;
+import com.fullcycle.admin.catalogo.infrastructure.services.StorageService;
+import com.fullcycle.admin.catalogo.infrastructure.services.impl.GCStorageService;
+import com.fullcycle.admin.catalogo.infrastructure.services.local.InMemoryStorageService;
+import com.google.cloud.storage.Storage;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class StorageConfig {
+
+  @Bean(name = "storageService")
+  @Profile({"production", "development"})
+  public StorageService gcStorageService(
+      final GoogleStorageProperties props, final Storage storage) {
+    return new GCStorageService(props.getBucket(), storage);
+  }
+
+  @Bean(name = "storageService")
+  @ConditionalOnMissingBean
+  public StorageService inMemoryStorageService() {
+    return new InMemoryStorageService();
+  }
+}
