2f53699612dd64823320b08b94c08dcab5ee41cd
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-07-06 08:30:14 -0300

    Testes unitários para gcstorageservice

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/services/local/InMemoryStorageServiceTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/services/local/InMemoryStorageServiceTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class InMemoryStorageAPITest {
+public class InMemoryStorageServiceTest {
 
   private InMemoryStorageService target = new InMemoryStorageService();
 
