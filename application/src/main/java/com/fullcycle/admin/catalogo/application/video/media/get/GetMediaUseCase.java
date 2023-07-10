08d5bb9aae7fa7e4c574ce67eb2ea8526ac3297f
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-07-10 08:27:43 -0300

    Implementando o caso de uso de get media by type

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/video/media/get/GetMediaUseCase.java
@@ -0,0 +1,5 @@
+package com.fullcycle.admin.catalogo.application.video.media.get;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+
+public abstract class GetMediaUseCase extends UseCase<GetMediaCommand, MediaOutput> {}
---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/video/media/get/MediaOutput.java
@@ -0,0 +1,9 @@
+package com.fullcycle.admin.catalogo.application.video.media.get;
+
+import com.fullcycle.admin.catalogo.domain.resource.Resource;
+
+public record MediaOutput(byte[] content, String contentType, String name) {
+  public static MediaOutput with(final Resource aResource) {
+    return new MediaOutput(aResource.content(), aResource.contentType(), aResource.name());
+  }
+}
