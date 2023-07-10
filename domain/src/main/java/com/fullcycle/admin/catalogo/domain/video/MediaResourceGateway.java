fc790a46215b8c459addaabcf2c2d22000699c0f
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-07-10 08:07:51 -0300

    Testes unitários do caso de uso de get media by type

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/video/MediaResourceGateway.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/video/MediaResourceGateway.java
@@ -1,11 +1,17 @@
 package com.fullcycle.admin.catalogo.domain.video;
 
 
+import com.fullcycle.admin.catalogo.domain.resource.Resource;
+
+import java.util.Optional;
+
 public interface MediaResourceGateway {
 
     AudioVideoMedia storeAudioVideo(VideoID anId, VideoResource aResource);
 
     ImageMedia storeImage(VideoID anId, VideoResource aResource);
+
+    Optional<Resource> getResource(VideoID anId, VideoMediaType type);
 
     void clearResources(VideoID anId);
 }
