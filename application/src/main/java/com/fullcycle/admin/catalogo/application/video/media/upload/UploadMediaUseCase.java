559bbf75f6c02f90fa9a619a2c8c00bb60b03346
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-07-11 09:03:31 -0300

    Implementando o caso de uso de upload media by type

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/video/media/upload/UploadMediaUseCase.java
@@ -0,0 +1,5 @@
+package com.fullcycle.admin.catalogo.application.video.media.upload;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+
+public abstract class UploadMediaUseCase extends UseCase<UploadMediaCommand, UploadMediaOutput> {}
--- application/src/test/java/com/fullcycle/admin/catalogo/application/video/media/upload/UploadMediaUseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/video/media/upload/UploadMediaUseCaseTest.java
@@ -1,5 +1,7 @@
 package com.fullcycle.admin.catalogo.application.video.media.upload;
 
+import static com.fullcycle.admin.catalogo.domain.video.VideoMediaType.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -21,248 +23,240 @@ import org.mockito.Mock;
 
 public class UploadMediaUseCaseTest extends UseCaseTest {
 
-    @InjectMocks
-    private DefaultUploadMediaUseCase useCase;
+  @InjectMocks private DefaultUploadMediaUseCase useCase;
 
-    @Mock
-    private MediaResourceGateway mediaResourceGateway;
+  @Mock private MediaResourceGateway mediaResourceGateway;
 
-    @Mock
-    private VideoGateway videoGateway;
+  @Mock private VideoGateway videoGateway;
 
-    @Override
-    protected List<Object> getMocks() {
-        return List.of(mediaResourceGateway, videoGateway);
-    }
+  @Override
+  protected List<Object> getMocks() {
+    return List.of(mediaResourceGateway, videoGateway);
+  }
 
-    @Test
-    public void givenCmdToUpload_whenIsValid_shouldUpdateVideoMediaAndPersistIt() {
-        // given
-        final var aVideo = Fixture.Videos.systemDesign();
-        final var expectedId = aVideo.getId();
-        final var expectedType = VideoMediaType.VIDEO;
-        final var expectedResource = Fixture.Videos.resource(expectedType);
-        final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
-        final var expectedMedia = Fixture.Videos.audioVideo(expectedType);
+  @Test
+  public void givenCmdToUpload_whenIsValid_shouldUpdateVideoMediaAndPersistIt() {
+    // given
+    final var aVideo = Fixture.Videos.systemDesign();
+    final var expectedId = aVideo.getId();
+    final var expectedType = VIDEO;
+    final var expectedResource = Fixture.Videos.resource(expectedType);
+    final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
+    final var expectedMedia = Fixture.Videos.audioVideo(expectedType);
 
-        when(videoGateway.findById(any()))
-                .thenReturn(Optional.of(aVideo));
+    when(videoGateway.findById(any())).thenReturn(Optional.of(aVideo));
 
-        when(mediaResourceGateway.storeAudioVideo(any(), any()))
-                .thenReturn(expectedMedia);
+    when(mediaResourceGateway.storeAudioVideo(any(), any())).thenReturn(expectedMedia);
 
-        when(videoGateway.update(any()))
-                .thenAnswer(returnsFirstArg());
+    when(videoGateway.update(any())).thenAnswer(returnsFirstArg());
 
-        final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
+    final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
 
-        // when
-        final var actualOutput = useCase.execute(aCmd);
+    // when
+    final var actualOutput = useCase.execute(aCmd);
 
-        // then
-        Assertions.assertEquals(expectedType, actualOutput.mediaType());
-        Assertions.assertEquals(expectedId.getValue(), actualOutput.videoId());
+    // then
+    assertEquals(expectedType, actualOutput.mediaType());
+    assertEquals(expectedId.getValue(), actualOutput.videoId());
 
-        verify(videoGateway, times(1)).findById(eq(expectedId));
+    verify(videoGateway, times(1)).findById(eq(expectedId));
 
-        verify(mediaResourceGateway, times(1)).storeAudioVideo(eq(expectedId), eq(expectedVideoResource));
+    verify(mediaResourceGateway, times(1))
+        .storeAudioVideo(eq(expectedId), eq(expectedVideoResource));
 
-        verify(videoGateway, times(1)).update(argThat(actualVideo ->
-                Objects.equals(expectedMedia, actualVideo.getVideo().get())
+    verify(videoGateway, times(1))
+        .update(
+            argThat(
+                actualVideo ->
+                    Objects.equals(expectedMedia, actualVideo.getVideo().get())
                         && actualVideo.getTrailer().isEmpty()
                         && actualVideo.getBanner().isEmpty()
                         && actualVideo.getThumbnail().isEmpty()
-                        && actualVideo.getThumbnailHalf().isEmpty()
-        ));
-    }
+                        && actualVideo.getThumbnailHalf().isEmpty()));
+  }
 
-    @Test
-    public void givenCmdToUpload_whenIsValid_shouldUpdateTrailerMediaAndPersistIt() {
-        // given
-        final var aVideo = Fixture.Videos.systemDesign();
-        final var expectedId = aVideo.getId();
-        final var expectedType = VideoMediaType.TRAILER;
-        final var expectedResource = Fixture.Videos.resource(expectedType);
-        final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
-        final var expectedMedia = Fixture.Videos.audioVideo(expectedType);
+  @Test
+  public void givenCmdToUpload_whenIsValid_shouldUpdateTrailerMediaAndPersistIt() {
+    // given
+    final var aVideo = Fixture.Videos.systemDesign();
+    final var expectedId = aVideo.getId();
+    final var expectedType = TRAILER;
+    final var expectedResource = Fixture.Videos.resource(expectedType);
+    final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
+    final var expectedMedia = Fixture.Videos.audioVideo(expectedType);
 
-        when(videoGateway.findById(any()))
-                .thenReturn(Optional.of(aVideo));
+    when(videoGateway.findById(any())).thenReturn(Optional.of(aVideo));
 
-        when(mediaResourceGateway.storeAudioVideo(any(), any()))
-                .thenReturn(expectedMedia);
+    when(mediaResourceGateway.storeAudioVideo(any(), any())).thenReturn(expectedMedia);
 
-        when(videoGateway.update(any()))
-                .thenAnswer(returnsFirstArg());
+    when(videoGateway.update(any())).thenAnswer(returnsFirstArg());
 
-        final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
+    final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
 
-        // when
-        final var actualOutput = useCase.execute(aCmd);
+    // when
+    final var actualOutput = useCase.execute(aCmd);
 
-        // then
-        Assertions.assertEquals(expectedType, actualOutput.mediaType());
-        Assertions.assertEquals(expectedId.getValue(), actualOutput.videoId());
+    // then
+    assertEquals(expectedType, actualOutput.mediaType());
+    assertEquals(expectedId.getValue(), actualOutput.videoId());
 
-        verify(videoGateway, times(1)).findById(eq(expectedId));
+    verify(videoGateway, times(1)).findById(eq(expectedId));
 
-        verify(mediaResourceGateway, times(1)).storeAudioVideo(eq(expectedId), eq(expectedVideoResource));
+    verify(mediaResourceGateway, times(1))
+        .storeAudioVideo(eq(expectedId), eq(expectedVideoResource));
 
-        verify(videoGateway, times(1)).update(argThat(actualVideo ->
-                actualVideo.getVideo().isEmpty()
+    verify(videoGateway, times(1))
+        .update(
+            argThat(
+                actualVideo ->
+                    actualVideo.getVideo().isEmpty()
                         && Objects.equals(expectedMedia, actualVideo.getTrailer().get())
                         && actualVideo.getBanner().isEmpty()
                         && actualVideo.getThumbnail().isEmpty()
-                        && actualVideo.getThumbnailHalf().isEmpty()
-        ));
-    }
+                        && actualVideo.getThumbnailHalf().isEmpty()));
+  }
 
-    @Test
-    public void givenCmdToUpload_whenIsValid_shouldUpdateBannerMediaAndPersistIt() {
-        // given
-        final var aVideo = Fixture.Videos.systemDesign();
-        final var expectedId = aVideo.getId();
-        final var expectedType = VideoMediaType.BANNER;
-        final var expectedResource = Fixture.Videos.resource(expectedType);
-        final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
-        final var expectedMedia = Fixture.Videos.image(expectedType);
+  @Test
+  public void givenCmdToUpload_whenIsValid_shouldUpdateBannerMediaAndPersistIt() {
+    // given
+    final var aVideo = Fixture.Videos.systemDesign();
+    final var expectedId = aVideo.getId();
+    final var expectedType = BANNER;
+    final var expectedResource = Fixture.Videos.resource(expectedType);
+    final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
+    final var expectedMedia = Fixture.Videos.image(expectedType);
 
-        when(videoGateway.findById(any()))
-                .thenReturn(Optional.of(aVideo));
+    when(videoGateway.findById(any())).thenReturn(Optional.of(aVideo));
 
-        when(mediaResourceGateway.storeImage(any(), any()))
-                .thenReturn(expectedMedia);
+    when(mediaResourceGateway.storeImage(any(), any())).thenReturn(expectedMedia);
 
-        when(videoGateway.update(any()))
-                .thenAnswer(returnsFirstArg());
+    when(videoGateway.update(any())).thenAnswer(returnsFirstArg());
 
-        final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
+    final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
 
-        // when
-        final var actualOutput = useCase.execute(aCmd);
+    // when
+    final var actualOutput = useCase.execute(aCmd);
 
-        // then
-        Assertions.assertEquals(expectedType, actualOutput.mediaType());
-        Assertions.assertEquals(expectedId.getValue(), actualOutput.videoId());
+    // then
+    assertEquals(expectedType, actualOutput.mediaType());
+    assertEquals(expectedId.getValue(), actualOutput.videoId());
 
-        verify(videoGateway, times(1)).findById(eq(expectedId));
+    verify(videoGateway, times(1)).findById(eq(expectedId));
 
-        verify(mediaResourceGateway, times(1)).storeImage(eq(expectedId), eq(expectedVideoResource));
+    verify(mediaResourceGateway, times(1)).storeImage(eq(expectedId), eq(expectedVideoResource));
 
-        verify(videoGateway, times(1)).update(argThat(actualVideo ->
-                actualVideo.getVideo().isEmpty()
+    verify(videoGateway, times(1))
+        .update(
+            argThat(
+                actualVideo ->
+                    actualVideo.getVideo().isEmpty()
                         && actualVideo.getTrailer().isEmpty()
                         && Objects.equals(expectedMedia, actualVideo.getBanner().get())
                         && actualVideo.getThumbnail().isEmpty()
-                        && actualVideo.getThumbnailHalf().isEmpty()
-        ));
-    }
+                        && actualVideo.getThumbnailHalf().isEmpty()));
+  }
 
-    @Test
-    public void givenCmdToUpload_whenIsValid_shouldUpdateThumbnailMediaAndPersistIt() {
-        // given
-        final var aVideo = Fixture.Videos.systemDesign();
-        final var expectedId = aVideo.getId();
-        final var expectedType = VideoMediaType.THUMBNAIL;
-        final var expectedResource = Fixture.Videos.resource(expectedType);
-        final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
-        final var expectedMedia = Fixture.Videos.image(expectedType);
+  @Test
+  public void givenCmdToUpload_whenIsValid_shouldUpdateThumbnailMediaAndPersistIt() {
+    // given
+    final var aVideo = Fixture.Videos.systemDesign();
+    final var expectedId = aVideo.getId();
+    final var expectedType = THUMBNAIL;
+    final var expectedResource = Fixture.Videos.resource(expectedType);
+    final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
+    final var expectedMedia = Fixture.Videos.image(expectedType);
 
-        when(videoGateway.findById(any()))
-                .thenReturn(Optional.of(aVideo));
+    when(videoGateway.findById(any())).thenReturn(Optional.of(aVideo));
 
-        when(mediaResourceGateway.storeImage(any(), any()))
-                .thenReturn(expectedMedia);
+    when(mediaResourceGateway.storeImage(any(), any())).thenReturn(expectedMedia);
 
-        when(videoGateway.update(any()))
-                .thenAnswer(returnsFirstArg());
+    when(videoGateway.update(any())).thenAnswer(returnsFirstArg());
 
-        final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
+    final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
 
-        // when
-        final var actualOutput = useCase.execute(aCmd);
+    // when
+    final var actualOutput = useCase.execute(aCmd);
 
-        // then
-        Assertions.assertEquals(expectedType, actualOutput.mediaType());
-        Assertions.assertEquals(expectedId.getValue(), actualOutput.videoId());
+    // then
+    assertEquals(expectedType, actualOutput.mediaType());
+    assertEquals(expectedId.getValue(), actualOutput.videoId());
 
-        verify(videoGateway, times(1)).findById(eq(expectedId));
+    verify(videoGateway, times(1)).findById(eq(expectedId));
 
-        verify(mediaResourceGateway, times(1)).storeImage(eq(expectedId), eq(expectedVideoResource));
+    verify(mediaResourceGateway, times(1)).storeImage(eq(expectedId), eq(expectedVideoResource));
 
-        verify(videoGateway, times(1)).update(argThat(actualVideo ->
-                actualVideo.getVideo().isEmpty()
+    verify(videoGateway, times(1))
+        .update(
+            argThat(
+                actualVideo ->
+                    actualVideo.getVideo().isEmpty()
                         && actualVideo.getTrailer().isEmpty()
                         && actualVideo.getBanner().isEmpty()
                         && Objects.equals(expectedMedia, actualVideo.getThumbnail().get())
-                        && actualVideo.getThumbnailHalf().isEmpty()
-        ));
-    }
+                        && actualVideo.getThumbnailHalf().isEmpty()));
+  }
 
-    @Test
-    public void givenCmdToUpload_whenIsValid_shouldUpdateThumbnailHalfMediaAndPersistIt() {
-        // given
-        final var aVideo = Fixture.Videos.systemDesign();
-        final var expectedId = aVideo.getId();
-        final var expectedType = VideoMediaType.THUMBNAIL_HALF;
-        final var expectedResource = Fixture.Videos.resource(expectedType);
-        final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
-        final var expectedMedia = Fixture.Videos.image(expectedType);
+  @Test
+  public void givenCmdToUpload_whenIsValid_shouldUpdateThumbnailHalfMediaAndPersistIt() {
+    // given
+    final var aVideo = Fixture.Videos.systemDesign();
+    final var expectedId = aVideo.getId();
+    final var expectedType = THUMBNAIL_HALF;
+    final var expectedResource = Fixture.Videos.resource(expectedType);
+    final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
+    final var expectedMedia = Fixture.Videos.image(expectedType);
 
-        when(videoGateway.findById(any()))
-                .thenReturn(Optional.of(aVideo));
+    when(videoGateway.findById(any())).thenReturn(Optional.of(aVideo));
 
-        when(mediaResourceGateway.storeImage(any(), any()))
-                .thenReturn(expectedMedia);
+    when(mediaResourceGateway.storeImage(any(), any())).thenReturn(expectedMedia);
 
-        when(videoGateway.update(any()))
-                .thenAnswer(returnsFirstArg());
+    when(videoGateway.update(any())).thenAnswer(returnsFirstArg());
 
-        final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
+    final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
 
-        // when
-        final var actualOutput = useCase.execute(aCmd);
+    // when
+    final var actualOutput = useCase.execute(aCmd);
 
-        // then
-        Assertions.assertEquals(expectedType, actualOutput.mediaType());
-        Assertions.assertEquals(expectedId.getValue(), actualOutput.videoId());
+    // then
+    assertEquals(expectedType, actualOutput.mediaType());
+    assertEquals(expectedId.getValue(), actualOutput.videoId());
 
-        verify(videoGateway, times(1)).findById(eq(expectedId));
+    verify(videoGateway, times(1)).findById(eq(expectedId));
 
-        verify(mediaResourceGateway, times(1)).storeImage(eq(expectedId), eq(expectedVideoResource));
+    verify(mediaResourceGateway, times(1)).storeImage(eq(expectedId), eq(expectedVideoResource));
 
-        verify(videoGateway, times(1)).update(argThat(actualVideo ->
-                actualVideo.getVideo().isEmpty()
+    verify(videoGateway, times(1))
+        .update(
+            argThat(
+                actualVideo ->
+                    actualVideo.getVideo().isEmpty()
                         && actualVideo.getTrailer().isEmpty()
                         && actualVideo.getBanner().isEmpty()
                         && actualVideo.getThumbnail().isEmpty()
-                        && Objects.equals(expectedMedia, actualVideo.getThumbnailHalf().get())
-        ));
-    }
+                        && Objects.equals(expectedMedia, actualVideo.getThumbnailHalf().get())));
+  }
 
-    @Test
-    public void givenCmdToUpload_whenVideoIsInvalid_shouldReturnNotFound() {
-        // given
-        final var aVideo = Fixture.Videos.systemDesign();
-        final var expectedId = aVideo.getId();
-        final var expectedType = VideoMediaType.THUMBNAIL_HALF;
-        final var expectedResource = Fixture.Videos.resource(expectedType);
-        final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
+  @Test
+  public void givenCmdToUpload_whenVideoIsInvalid_shouldReturnNotFound() {
+    // given
+    final var aVideo = Fixture.Videos.systemDesign();
+    final var expectedId = aVideo.getId();
+    final var expectedType = THUMBNAIL_HALF;
+    final var expectedResource = Fixture.Videos.resource(expectedType);
+    final var expectedVideoResource = VideoResource.with(expectedType, expectedResource);
 
-        final var expectedErrorMessage = "Video with ID %s was not found".formatted(expectedId.getValue());
+    final var expectedErrorMessage =
+        "Video with ID %s was not found".formatted(expectedId.getValue());
 
-        when(videoGateway.findById(any()))
-                .thenReturn(Optional.empty());
+    when(videoGateway.findById(any())).thenReturn(Optional.empty());
 
-        final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
+    final var aCmd = UploadMediaCommand.with(expectedId.getValue(), expectedVideoResource);
 
-        // when
-        final var actualException = Assertions.assertThrows(
-                NotFoundException.class,
-                () -> useCase.execute(aCmd)
-        );
+    // when
+    final var actualException =
+        Assertions.assertThrows(NotFoundException.class, () -> useCase.execute(aCmd));
 
-        // then
-        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
-    }
+    // then
+    assertEquals(expectedErrorMessage, actualException.getMessage());
+  }
 }
