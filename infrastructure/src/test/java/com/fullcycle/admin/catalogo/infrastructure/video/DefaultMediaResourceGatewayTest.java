73a6d8654091fa15258f7b997a8492fefab9d073
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-07-11 08:50:07 -0300

    Testes unitários do caso de uso de upload media by type

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/video/DefaultMediaResourceGatewayTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/video/DefaultMediaResourceGatewayTest.java
@@ -4,6 +4,8 @@ import static com.fullcycle.admin.catalogo.domain.Fixture.Videos.mediaType;
 import static com.fullcycle.admin.catalogo.domain.Fixture.Videos.resource;
 import static com.fullcycle.admin.catalogo.domain.video.MediaStatus.PENDING;
 import static com.fullcycle.admin.catalogo.domain.video.VideoMediaType.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fullcycle.admin.catalogo.IntegrationTest;
 import com.fullcycle.admin.catalogo.domain.video.*;
@@ -54,15 +56,15 @@ class DefaultMediaResourceGatewayTest {
 
     // then
     Assertions.assertNotNull(actualMedia.id());
-    Assertions.assertEquals(expectedLocation, actualMedia.rawLocation());
-    Assertions.assertEquals(expectedResource.name(), actualMedia.name());
-    Assertions.assertEquals(expectedResource.checksum(), actualMedia.checksum());
-    Assertions.assertEquals(expectedStatus, actualMedia.status());
-    Assertions.assertEquals(expectedEncodedLocation, actualMedia.encodedLocation());
+    assertEquals(expectedLocation, actualMedia.rawLocation());
+    assertEquals(expectedResource.name(), actualMedia.name());
+    assertEquals(expectedResource.checksum(), actualMedia.checksum());
+    assertEquals(expectedStatus, actualMedia.status());
+    assertEquals(expectedEncodedLocation, actualMedia.encodedLocation());
 
     final var actualStored = storageService().storage().get(expectedLocation);
 
-    Assertions.assertEquals(expectedResource, actualStored);
+    assertEquals(expectedResource, actualStored);
   }
 
   @Test
@@ -81,68 +83,68 @@ class DefaultMediaResourceGatewayTest {
 
     // then
     Assertions.assertNotNull(actualMedia.id());
-    Assertions.assertEquals(expectedLocation, actualMedia.location());
-    Assertions.assertEquals(expectedResource.name(), actualMedia.name());
-    Assertions.assertEquals(expectedResource.checksum(), actualMedia.checksum());
+    assertEquals(expectedLocation, actualMedia.location());
+    assertEquals(expectedResource.name(), actualMedia.name());
+    assertEquals(expectedResource.checksum(), actualMedia.checksum());
 
     final var actualStored = storageService().storage().get(expectedLocation);
 
-    Assertions.assertEquals(expectedResource, actualStored);
+    assertEquals(expectedResource, actualStored);
   }
 
   @Test
   public void givenValidVideoId_whenCallsGetResource_shouldReturnIt() {
     // given
     final var videoOne = VideoID.unique();
-    final var expectedType = VideoMediaType.VIDEO;
+    final var expectedType = VIDEO;
     final var expectedResource = resource(expectedType);
 
     storageService()
         .store("videoId-%s/type-%s".formatted(videoOne.getValue(), expectedType), expectedResource);
     storageService()
         .store(
-            "videoId-%s/type-%s".formatted(videoOne.getValue(), VideoMediaType.TRAILER.name()),
+            "videoId-%s/type-%s".formatted(videoOne.getValue(), TRAILER.name()),
             resource(mediaType()));
     storageService()
         .store(
-            "videoId-%s/type-%s".formatted(videoOne.getValue(), VideoMediaType.BANNER.name()),
+            "videoId-%s/type-%s".formatted(videoOne.getValue(), BANNER.name()),
             resource(mediaType()));
 
-    Assertions.assertEquals(3, storageService().storage().size());
+    assertEquals(3, storageService().storage().size());
 
     // when
     final var actualResult = this.mediaResourceGateway.getResource(videoOne, expectedType).get();
 
     // then
-    Assertions.assertEquals(expectedResource, actualResult);
+    assertEquals(expectedResource, actualResult);
   }
 
   @Test
   public void givenInvalidType_whenCallsGetResource_shouldReturnEmpty() {
     // given
     final var videoOne = VideoID.unique();
-    final var expectedType = VideoMediaType.THUMBNAIL;
+    final var expectedType = THUMBNAIL;
 
     storageService()
         .store(
-            "videoId-%s/type-%s".formatted(videoOne.getValue(), VideoMediaType.VIDEO.name()),
+            "videoId-%s/type-%s".formatted(videoOne.getValue(), VIDEO.name()),
             resource(mediaType()));
     storageService()
         .store(
-            "videoId-%s/type-%s".formatted(videoOne.getValue(), VideoMediaType.TRAILER.name()),
+            "videoId-%s/type-%s".formatted(videoOne.getValue(), TRAILER.name()),
             resource(mediaType()));
     storageService()
         .store(
-            "videoId-%s/type-%s".formatted(videoOne.getValue(), VideoMediaType.BANNER.name()),
+            "videoId-%s/type-%s".formatted(videoOne.getValue(), BANNER.name()),
             resource(mediaType()));
 
-    Assertions.assertEquals(3, storageService().storage().size());
+    assertEquals(3, storageService().storage().size());
 
     // when
     final var actualResult = this.mediaResourceGateway.getResource(videoOne, expectedType);
 
     // then
-    Assertions.assertTrue(actualResult.isEmpty());
+    assertTrue(actualResult.isEmpty());
   }
 
   @Test
@@ -163,17 +165,17 @@ class DefaultMediaResourceGatewayTest {
     toBeDeleted.forEach(id -> storageService().store(id, resource(mediaType())));
     expectedValues.forEach(id -> storageService().store(id, resource(mediaType())));
 
-    Assertions.assertEquals(5, storageService().storage().size());
+    assertEquals(5, storageService().storage().size());
 
     // when
     this.mediaResourceGateway.clearResources(videoOne);
 
     // then
-    Assertions.assertEquals(2, storageService().storage().size());
+    assertEquals(2, storageService().storage().size());
 
     final var actualKeys = storageService().storage().keySet();
 
-    Assertions.assertTrue(
+    assertTrue(
         expectedValues.size() == actualKeys.size() && actualKeys.containsAll(expectedValues));
   }
 
