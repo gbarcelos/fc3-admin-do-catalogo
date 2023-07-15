a78c17724bd9c04996a290e1625d67455a4d8b29
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-07-15 18:16:07 -0300

    Qualificando os beans para injeção do spring

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/AmqpConfig.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/AmqpConfig.java
@@ -1,5 +1,7 @@
 package com.fullcycle.admin.catalogo.infrastructure.configuration;
 
+import com.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoCreatedQueue;
+import com.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoEncodedQueue;
 import com.fullcycle.admin.catalogo.infrastructure.configuration.properties.amqp.QueueProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -8,15 +10,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AmqpConfig {
 
-    @Bean
-    @ConfigurationProperties("amqp.queues.video-created")
-    public QueueProperties videoCreatedQueueProperties() {
-        return new QueueProperties();
-    }
+  @Bean
+  @ConfigurationProperties("amqp.queues.video-created")
+  @VideoCreatedQueue
+  public QueueProperties videoCreatedQueueProperties() {
+    return new QueueProperties();
+  }
 
-    @Bean
-    @ConfigurationProperties("amqp.queues.video-encoded")
-    public QueueProperties videoEncodedQueueProperties() {
-        return new QueueProperties();
-    }
+  @Bean
+  @ConfigurationProperties("amqp.queues.video-encoded")
+  @VideoEncodedQueue
+  public QueueProperties videoEncodedQueueProperties() {
+    return new QueueProperties();
+  }
 }
