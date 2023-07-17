d1bdd08edf6352072e155441998ff7621da97418
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-07-16 22:42:16 -0300

    Adicionando domain events no domínio de video

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/AggregateRoot.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/AggregateRoot.java
@@ -1,7 +1,14 @@
 package com.fullcycle.admin.catalogo.domain;
 
+import com.fullcycle.admin.catalogo.domain.events.DomainEvent;
+import java.util.List;
+
 public abstract class AggregateRoot<ID extends Identifier> extends Entity<ID> {
   protected AggregateRoot(ID id) {
     super(id);
+  }
+
+  protected AggregateRoot(final ID id, final List<DomainEvent> events) {
+    super(id, events);
   }
 }
