c483913537d8cb834495ff95e9122205a8273f01
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-07-16 23:09:39 -0300

    Testes unitários de domain events na classe entity

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/events/DomainEventPublisher.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/events/DomainEventPublisher.java
@@ -2,5 +2,5 @@ package com.fullcycle.admin.catalogo.domain.events;
 
 @FunctionalInterface
 public interface DomainEventPublisher {
-  <T extends DomainEvent> void publishEvent(T event);
+  void publishEvent(DomainEvent event);
 }
---
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/EntityTest.java
@@ -0,0 +1,127 @@
+package com.fullcycle.admin.catalogo.domain;
+
+import static com.fullcycle.admin.catalogo.domain.utils.IdUtils.uuid;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.fullcycle.admin.catalogo.domain.events.DomainEvent;
+import com.fullcycle.admin.catalogo.domain.utils.InstantUtils;
+import com.fullcycle.admin.catalogo.domain.validation.ValidationHandler;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EntityTest {
+
+  @Test
+  public void givenNullAsEvents_whenInstantiate_shouldBeOk() {
+
+    // given
+    final List<DomainEvent> events = null;
+
+    // when
+    final var anEntity = new DummyEntity(new DummyID(), events);
+
+    // then
+    assertNotNull(anEntity.getDomainEvents());
+    assertTrue(anEntity.getDomainEvents().isEmpty());
+  }
+
+  @Test
+  public void givenDomainEvents_whenPassInConstructor_shouldCreateADefensiveClone() {
+
+    // given
+    final List<DomainEvent> events = new ArrayList<>();
+    events.add(new DummyEvent());
+
+    // when
+    final var anEntity = new DummyEntity(new DummyID(), events);
+
+    // then
+    Assertions.assertNotNull(anEntity.getDomainEvents());
+    Assertions.assertEquals(1, anEntity.getDomainEvents().size());
+
+    Assertions.assertThrows(
+        RuntimeException.class,
+        () -> {
+          final var actualEvents = anEntity.getDomainEvents();
+          actualEvents.add(new DummyEvent());
+        });
+  }
+
+  @Test
+  public void givenEmptyDomainEvents_whenCallsRegisterEvent_shouldAddEventToList() {
+
+    // given
+    final var expectedEvents = 1;
+    final var anEntity = new DummyEntity(new DummyID(), new ArrayList<>());
+
+    // when
+    anEntity.registerEvent(new DummyEvent());
+
+    // then
+    Assertions.assertNotNull(anEntity.getDomainEvents());
+    Assertions.assertEquals(expectedEvents, anEntity.getDomainEvents().size());
+  }
+
+  @Test
+  public void givenAFewDomainEvents_whenCallsPublishEvents_shouldCallPublisherAndClearTheList() {
+
+    // given
+    final var expectedEvents = 0;
+    final var expectedSentEvents = 2;
+    final var counter = new AtomicInteger(0);
+    final var anEntity = new DummyEntity(new DummyID(), new ArrayList<>());
+
+    anEntity.registerEvent(new DummyEvent());
+    anEntity.registerEvent(new DummyEvent());
+
+    Assertions.assertEquals(2, anEntity.getDomainEvents().size());
+
+    // when
+    anEntity.publishDomainEvents(
+        event -> {
+          counter.incrementAndGet();
+        });
+
+    // then
+    Assertions.assertNotNull(anEntity.getDomainEvents());
+    Assertions.assertEquals(expectedEvents, anEntity.getDomainEvents().size());
+    Assertions.assertEquals(expectedSentEvents, counter.get());
+  }
+
+  public static class DummyEvent implements DomainEvent {
+
+    @Override
+    public Instant occurredOn() {
+      return InstantUtils.now();
+    }
+  }
+
+  public static class DummyID extends Identifier {
+
+    private final String id;
+
+    public DummyID() {
+      this.id = uuid();
+    }
+
+    @Override
+    public String getValue() {
+      return this.id;
+    }
+  }
+
+  public static class DummyEntity extends Entity<DummyID> {
+
+    public DummyEntity(DummyID dummyID, List<DomainEvent> domainEvents) {
+      super(dummyID, domainEvents);
+    }
+
+    @Override
+    public void validate(ValidationHandler handler) {}
+  }
+}
