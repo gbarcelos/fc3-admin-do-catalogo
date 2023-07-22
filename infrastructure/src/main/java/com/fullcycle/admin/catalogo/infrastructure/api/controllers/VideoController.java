02adb1f6dfd0dc002a27e1900cac567ece3fea9d
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-07-21 23:36:31 -0300

    Criando controller de video

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/VideoController.java
@@ -0,0 +1,7 @@
+package com.fullcycle.admin.catalogo.infrastructure.api.controllers;
+
+import com.fullcycle.admin.catalogo.infrastructure.api.VideoAPI;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VideoController implements VideoAPI {}
---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/api/VideoAPITest.java
@@ -0,0 +1,8 @@
+package com.fullcycle.admin.catalogo.infrastructure.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.fullcycle.admin.catalogo.ControllerTest;
+
+@ControllerTest(controllers = VideoAPI.class)
+public class VideoAPITest {}
