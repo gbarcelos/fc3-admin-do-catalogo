17fb1677adf191e5a8080c01666a5765414808ad
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-07-27 22:12:13 -0300

    Implementando o endpoint de delete de vídeo por id

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/VideoAPI.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/VideoAPI.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Set;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -80,4 +81,14 @@ public interface VideoAPI {
       })
   ResponseEntity<?> update(
       @PathVariable(name = "id") String id, @RequestBody UpdateVideoRequest payload);
+
+  @DeleteMapping(value = "{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @Operation(summary = "Delete a video by it's identifier")
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "204", description = "Video deleted"),
+        @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+      })
+  void deleteById(@PathVariable(name = "id") String id);
 }
