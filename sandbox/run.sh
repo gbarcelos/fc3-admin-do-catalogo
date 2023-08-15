e1580bce72c60ae227919a9155108343aad7f6b7
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-08-15 15:10:22 -0300

    Criando a estrutura de sandbox com docker

---
+++ sandbox/run.sh
@@ -0,0 +1,10 @@
+
+# Criar as pastas com permissões
+mkdir -m 777 .docker
+mkdir -m 777 .docker/mysql
+mkdir -m 777 .docker/keycloak
+
+docker compose -f app/docker-compose.yml up -d
+
+echo "Inicializando os containers..."
+sleep 20
