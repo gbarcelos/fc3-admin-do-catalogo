7f6ce98ee39a08fffef6bc11887164bf1a4365c4
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-08-17 09:43:20 -0300

    Adicionando o elasticsearch e a docker network elastic

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,12 +1,16 @@
 # Criar as docker networks
 docker network create adm_videos_services
+docker network create elastic
 
 # Criar as pastas com permissões
 mkdir -m 777 .docker
 mkdir -m 777 .docker/mysql
 mkdir -m 777 .docker/keycloak
+mkdir -m 777 .docker/es01
 
-docker compose -f app/docker-compose.yml up -d
+docker compose -f services/docker-compose.yml up -d
+docker compose -f elk/docker-compose.yml up -d
+# docker compose -f app/docker-compose.yml up -d
 
 echo "Inicializando os containers..."
 sleep 20
