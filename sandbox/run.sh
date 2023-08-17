bf4d7eca07a3f41e7871be1bd144ecb18d682061
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-08-17 10:32:11 -0300

    Adicionando o filebeat

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -3,12 +3,14 @@ docker network create adm_videos_services
 docker network create elastic
 
 # Criar as pastas com permissões
+sudo chown root app/filebeat/filebeat.docker.yml
 mkdir -m 777 .docker
 mkdir -m 777 .docker/mysql
 mkdir -m 777 .docker/keycloak
 mkdir -m 777 .docker/es01
+mkdir -m 777 .docker/filebeat
 
-docker compose -f services/docker-compose.yml up -d
+# docker compose -f services/docker-compose.yml up -d
 docker compose -f elk/docker-compose.yml up -d
 # docker compose -f app/docker-compose.yml up -d
 
